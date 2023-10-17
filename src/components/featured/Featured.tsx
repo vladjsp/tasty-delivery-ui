35ebf0a8711008f406a6f78a9bb26ad2e3c10734
vladjsp <vladjsp@gmail.com>
2023-10-17 19:40:40 +0300

    create featured products

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './styles.module.css'
+import { featuredProducts } from '@/data'
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        { featuredProducts.map((item) => (
+                  <div key={item.id} className={styles.productCard}>
+                  <div className={styles.imageWrapper}>
+                    <Image src='/images/p1.png' alt='' fill  className={styles.image} />
+                  </div>
+                  <div className={styles.info}>
+                    <h2 className={styles.title}>{item.title}</h2>
+                    <p className={styles.description}>{item.desc}</p>
+                    <span className={styles.price}>${item.price}</span>
+                    <button className={styles.button}>Add to Cart</button>
+                  </div>
+                </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
