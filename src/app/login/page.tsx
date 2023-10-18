ca6d20fdcc55005acfde39046f25d7d120fc99b7
vladjsp <vladjsp@gmail.com>
2023-10-18 16:35:02 +0300

    create login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from './styles.module.css'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className={styles.container}>
+    {/* BOX */}
+    <div className={styles.wrapper}>
+      {/* IMAGE CONTAINER */}
+      <div className={styles.imageBox}>
+        <Image src="/images/loginBg.png" alt="" fill style={{objectFit: 'cover'}} />
+      </div>
+      {/* FORM CONTAINER */}
+      <div className={styles.form}>
+        <h1 className={styles.title}>Welcome</h1>
+        <p>Log into your account or create a new one using social buttons</p>
+        <button className={styles.signInBtn}>
+          <Image
+            src="/images/google.png"
+            alt=""
+            width={20}
+            height={20}
+            style={{objectFit: 'contain'}}
+          />
+          <span>Sign in with Google</span>
+        </button>
+        <button className={styles.signInBtn}>
+          <Image
+            src="/images/facebook.png"
+            alt=""
+            width={20}
+            height={20}
+            style={{objectFit: 'contain'}}
+          />
+          <span>Sign in with Facebook</span>
+        </button>
+        <p className={styles.contactUs}>
+          {/* TODO: replace link to actual contacts */}
+          Have a problem? <Link href="#">Contact us</Link>
+        </p>
+      </div>
+    </div>
+  </div>
   )
 }
 
