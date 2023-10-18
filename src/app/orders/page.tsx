cb083afafc510ea7e1d8ac3e713e4ec65d107308
vladjsp <vladjsp@gmail.com>
2023-10-18 17:03:32 +0300

    create orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import styles from './styles.module.css'
 
 const OrdersPage = () => {
   return (
-    <div>OrdersPage</div>
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <thead>
+          <tr style={{textAlign: 'left'}}>
+            <th className={styles.optionalColumn}>Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th className={styles.optionalColumn}>Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={styles.orderRow}>
+            <td>1237861238721</td>
+            <td className={styles.cell}>19.07.2023</td>
+            <td className={styles.cell}>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td className={styles.cell}>On the way (approx. 10min)...</td>
+          </tr>
+          <tr className={styles.orderRow}>
+            <td>1237861238721</td>
+            <td className={styles.cell}>19.07.2023</td>
+            <td className={styles.cell}>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td className={styles.cell}>On the way (approx. 10min)...</td>
+          </tr>
+          <tr className={styles.orderRow}>
+            <td>1237861238721</td>
+            <td className={styles.cell}>19.07.2023</td>
+            <td className={styles.cell}>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td className={styles.cell}>On the way (approx. 10min)...</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
   )
 }
 
